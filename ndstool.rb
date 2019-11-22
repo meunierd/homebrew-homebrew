3c825253411bc515dd1d1beb0be4a25af0588cdf
Devon Meunier <devon.meunier@shopify.com>
2019-11-22 08:32:17 -0500

    ndstool: Add description and homepage

--- ndstool.rb
+++ ndstool.rb
@@ -1,6 +1,6 @@
 class Ndstool < Formula
-  desc ""
-  homepage ""
+  desc "Utility for inspecting, extracting, and packing Nintendo DS ROMs"
+  homepage "https://www.darkfader.net/ds/"
   url "https://github.com/devkitPro/ndstool/archive/v2.1.2.tar.gz"
   head "https://github.com/devkitPro/ndstool.git"
   sha256 "e58196e1287edbf54a965b28b4b2e92c0df7c6d8e9e1a40fccc37c5aa05f9ebc"
