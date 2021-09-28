f24cef08f7591135048b10655d7592a75243017e
Devon Meunier <devon.meunier@shopify.com>
2021-09-28 09:10:32 -0400

    maxcso v1.13.0

--- Formula/maxcso.rb
+++ Formula/maxcso.rb
@@ -1,8 +1,8 @@
 class Maxcso < Formula
   desc "Fast cso compressor"
   homepage ""
-  url "https://github.com/unknownbrackets/maxcso/archive/v1.12.0.tar.gz"
-  sha256 "e55b3ff8da6337f23da0fd2c51ee31571aba536b18b0039b9353b61b6fec7cb3"
+  url "https://github.com/unknownbrackets/maxcso/archive/v1.13.0.tar.gz"
+  sha256 "af9c05add1a1d199ec184d3471081af1b91d591b2473800ea989c882fb632730"
 
   depends_on "lz4"
   depends_on "libuv"
